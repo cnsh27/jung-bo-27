9c6a86471d92246e8e0246d3e7ee22977239039b
cham_why <53082717+chamwhy@users.noreply.github.com>
2020-11-05 11:14:22 +0900

    Add files via upload

---
+++ Sequential_search.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define MAX 1<<5
+
+int d[MAX];
+int main(){
+	int s, n, i = 0;
+	scanf("%d", &s);
+	scanf("%d", &n);
+	for(int m = 1; m <= n; m++){
+		scanf("%d", &d[m]);
+	}
+	while(d[i] != s && i <= n) i++;
+	if(i == n){
+		printf("Not Found!");
+	}else{
+		printf("%d", i);
+	}
+}
